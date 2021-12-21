678b08123bfa3063b6cfc4b19f4d800c77283025
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2021-12-21 06:28:49 +0100

    Add files via upload

---
+++ trhacknodef.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#####DONT CHANGE THIS########
+######################
+### Script By INDOnimous
+### www.memanon.ml
+### Don't Change This.!!!
+######################
+import os
+import sys
+os.system("clear")
+os.system("mkdir TRHACKNOnimous")
+os.system("mv TRHACKNOnimous/ /storage/emulated/0/")
+os.system("mv TRHACKNONscript.html /storage/emulated/0/TRHACKNOnimous/")
+print
+print("Data Sedang Di pindahkan, Silahkan Tunggu.")
+os.system("sleep 5")
+print("Berhasil Di pindahkan ke /storage/emulated/0/TRHACKNOnimous/TRHACKNONscript.html")
+os.system("sleep 3")
+print("[ Script Siap Di gunakan ]")
