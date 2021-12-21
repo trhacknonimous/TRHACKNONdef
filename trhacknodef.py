a78055f143ec99b9be562a61e5cd1f3e75d32fef
trhacknonimous <82779890+trhacknonimous@users.noreply.github.com>
2021-12-21 06:30:52 +0100

    Update trhacknodef.py

--- trhacknodef.py
+++ trhacknodef.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #####DONT CHANGE THIS########
 ######################
-### Script By INDOnimous
+### Script By TRHACKNOnimous
 ### www.memanon.ml
 ### Don't Change This.!!!
 ######################
@@ -11,10 +11,10 @@ import sys
 os.system("clear")
 os.system("mkdir TRHACKNOnimous")
 os.system("mv TRHACKNOnimous/ /storage/emulated/0/")
-os.system("mv TRHACKNONscript.html /storage/emulated/0/TRHACKNOnimous/")
+os.system("cp TRHACKNONscript.html /storage/emulated/0/TRHACKNOnimous/")
 print
-print("Data Sedang Di pindahkan, Silahkan Tunggu.")
+print("tu n'as plus qu'à utiliser un outil comme trhacktest, pour uploader le script que tu viens de creer.")
 os.system("sleep 5")
-print("Berhasil Di pindahkan ke /storage/emulated/0/TRHACKNOnimous/TRHACKNONscript.html")
+print("script créé dans : /storage/emulated/0/TRHACKNOnimous/TRHACKNONscript.html")
 os.system("sleep 3")
-print("[ Script Siap Di gunakan ]")
+print("[ Script en cours de chargement ]")
